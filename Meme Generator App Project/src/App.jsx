9a4144144e7448fab669dc73d84a7fb1ff04123a
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-04-11 04:21:55 -0700

    file structure initialized; Header and Meme components createdd

---
+++ Meme Generator App Project/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header.jsx"
+import Meme from "./Meme.jsx"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
+}
+
+export default App
